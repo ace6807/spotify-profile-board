05d34d6bf2f006a67694d7f0db11cb09ee937b5f
Andrew Yatsko <ayatsko@live.com>
2021-12-31 19:50:53 -0500

    Add context processor for shell

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
+from app.db import db
 from app import create_app
 from config import Config
 
 app_config = Config()
 app = create_app(app_config)
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db}
