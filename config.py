f5b96cd73f840175f98eade1aa553faf25ba64b7
Andrew Yatsko <ayatsko@live.com>
2022-01-01 15:24:39 -0500

    Add Spotify config variables

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'development-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')  
+    SECRET_KEY = os.getenv('SECRET_KEY', 'development-key')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f"'sqlite:///{os.path.join(basedir, 'app.db')}")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-  
+    SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+    SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
