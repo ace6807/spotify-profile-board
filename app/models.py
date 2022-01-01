fb8352c9ba64a2be5f2b33c9199f1a049ed33f61
Andrew Yatsko <ayatsko@live.com>
2022-01-01 12:19:16 -0500

    Change column sizes on user model

--- app/models.py
+++ app/models.py
@@ -2,10 +2,10 @@ from app.db import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    first_name = db.Column(db.String(120), nullable=True)
-    last_name = db.Column(db.String(120), nullable=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    first_name = db.Column(db.String(50), nullable=True)
+    last_name = db.Column(db.String(50), nullable=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
