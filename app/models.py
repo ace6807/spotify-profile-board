b152ec9b5d6701e30d355196195e2cf009e881c3
Andrew Yatsko <ayatsko@live.com>
2022-01-01 15:24:49 -0500

    Add docs on user model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,14 @@
 from app.db import db
 
 class User(db.Model):
+    """User model
+
+    Fields:
+        username (str): Username [max 50]
+        email (str): Email [max 150]
+        first_name (str): First Name [max 50] (optional)
+        last_name (str): Last Name [max 50] (optional)
+    """
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
